a3554b3a0b1e400bae7e5e696dd81dfdc88d043e
Alfred Olofsson Johansson <alfred.olofsson-johansson@edu.nybro.se>
2022-07-15 23:23:35 +0200

    unit

---
+++ GameJam/Assets/Scripts/Unit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string unitName;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+
+}
