bad68e0b3b9ff480590d531bf9aae77f59dea26c
Alfred Olofsson Johansson <alfred.olofsson-johansson@edu.nybro.se>
2022-07-15 21:28:11 +0200

    paus nice yees

--- GameJam/Assets/Scripts/pauseMenu.cs
+++ GameJam/Assets/Scripts/pauseMenu.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class pauseMenu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,27 @@ public class pauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
