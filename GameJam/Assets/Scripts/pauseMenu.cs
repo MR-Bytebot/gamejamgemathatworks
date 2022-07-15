82213a625a47591a617a1c3c68cc4529a86d0a6a
Alfred Olofsson Johansson <alfred.olofsson-johansson@edu.nybro.se>
2022-07-15 21:41:26 +0200

    okhuvudmeny

--- GameJam/Assets/Scripts/pauseMenu.cs
+++ GameJam/Assets/Scripts/pauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pauseMenu : MonoBehaviour
 {
@@ -39,5 +40,9 @@ public class pauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+    public void mainMenuBack()
+    {
+        SceneManager.LoadScene(0);
     }
 }
