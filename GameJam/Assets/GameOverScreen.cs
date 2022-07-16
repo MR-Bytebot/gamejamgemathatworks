33f3e9a1ffee680ef2f537efa7ee66aac561703d
Edvinatschoool <Edvin.johansson@edu.nybro.se>
2022-07-16 18:30:54 +0200

    r

--- GameJam/Assets/GameOverScreen.cs
+++ GameJam/Assets/GameOverScreen.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class GameOverScreen : MonoBehaviour
 {
+<<<<<<< Updated upstream
    
+=======
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+>>>>>>> Stashed changes
 }
